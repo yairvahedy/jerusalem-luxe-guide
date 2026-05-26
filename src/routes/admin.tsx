81a3abe411e6cb5f8a0d5023fb69b642ffb6f04d
yairvahedy <59580298-yairvahedy@users.noreply.replit.com>
2026-05-26 18:50:52 +0000

    Restored to 'fd590aa0f6d5381e942e3d8370d444dcd528b243'
    Replit-Restored-To: fd590aa0f6d5381e942e3d8370d444dcd528b243

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -3,7 +3,7 @@ import { LayoutDashboard, List, Users, LogOut, Menu, X, Home, FileText, MapPin, 
 import { useState } from "react";
 import { useAuth } from "@/lib/auth";
 import { ToastProvider } from "@/lib/toast";
-import logo from "@/assets/jack-photo.png";
+import logo from "@/assets/jf-logo.jpeg";
 
 export const Route = createFileRoute("/admin")({
   component: AdminLayout,
